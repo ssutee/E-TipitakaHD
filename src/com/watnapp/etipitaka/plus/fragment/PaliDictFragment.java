f0282236ce4f402ea19ba7bfd80188914e2d8bf5
Sutee Sudprasert <etipitaka@gmail.com>
2013-07-15 13:14:15 +0700

    prepare Pali dict

---
+++ src/com/watnapp/etipitaka/plus/fragment/PaliDictFragment.java
@@ -0,0 +1,15 @@
+package com.watnapp.etipitaka.plus.fragment;
+
+import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockListFragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sutee
+ * Date: 15/7/2013
+ * Time: 12:53
+  */
+public class PaliDictFragment extends RoboSherlockListFragment {
+
+
+
+}
